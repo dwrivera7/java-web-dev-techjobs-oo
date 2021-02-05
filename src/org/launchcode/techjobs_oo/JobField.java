79cab93e50e5a47d35000c0f9efd943885a7ada8
Dathan Rivera <dwrivera7@gmail.com>
2021-02-04 20:09:51 -0600

    Fifth commit, JobField done, beginning removal of duplicate code.

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -1,4 +1,55 @@
 package org.launchcode.techjobs_oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField(){
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value){
+        this();
+        this.value = value;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+    @Override
+    public String toString(){
+        if(this.value == null || this.value.isEmpty()){
+            return "Data not available";
+        } else {
+            return this.value;
+        }
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField job_field = (JobField) o;
+        return getId() == job_field.getId();
+    }
+
 }
